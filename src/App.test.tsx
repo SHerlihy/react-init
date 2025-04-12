da77b7e429f9bc096278765927634d0251d033a3
SHerlihy <steven_herlihy@yahoo.com>
2025-04-12 20:46:35 +0100

    add user event testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,24 @@
 import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
 describe('App', () => {
-  it('renders the App component', () => {
-    render(<App />)
-    
-    screen.debug();
-  })
+    it('renders the App component', () => {
+        render(<App />)
+        screen.debug();
+    })
+
+    it('tab navigate to count button', async () => {
+        render(<App />)
+        const user = userEvent.setup()
+
+        const countButton = await screen.findByText(/count is \d+/)
+
+        while (countButton !== document.activeElement) {
+            await user.tab()
+        }
+
+        expect(document.activeElement).toBe(countButton)
+    })
 })
