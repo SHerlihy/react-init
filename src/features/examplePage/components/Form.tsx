f7a1e38498c7442a25db2801def6a2705d372165
SHerlihy <steven_herlihy@yahoo.com>
2025-04-14 17:01:45 +0100

    add display value from form request

--- src/features/examplePage/components/Form.tsx
+++ src/features/examplePage/components/Form.tsx
@@ -2,6 +2,12 @@ import { useForm } from '@tanstack/react-form'
 import { Input } from '@/components/ui/input'
 import z from 'zod'
 
+import {
+    QueryClient,
+    QueryClientProvider,
+    useMutation,
+} from '@tanstack/react-query'
+import { Button } from '@/components/ui/button'
 
 const berryTuple = ["cheri", "chesto", "pecha"] as const
 const inputValidationError = `Input must match ${berryTuple.join(" | ")}`
@@ -18,7 +24,28 @@ type BerryRes = {
 
 const BASE_URL = "https://pokeapi.co/api/v2/berry/"
 
+const queryClient = new QueryClient()
+
 function Form() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BerryWeightForm />
+        </QueryClientProvider>
+    )
+}
+
+function BerryWeightForm() {
+    const mutation = useMutation({
+        mutationFn: async (getUrl: string) => {
+            await new Promise((resolve) => setTimeout(resolve, 1000))
+            const berryRes = await fetch(getUrl).then((res) =>
+                res.json(),
+            ) as BerryRes
+
+            return berryRes.size
+        },
+    })
+
     const form = useForm({
         defaultValues: {
             berry: ''
@@ -27,42 +54,60 @@ function Form() {
             onChange: berrySchema
         },
         onSubmit: async ({ value }) => {
-            const response = await fetch(
-                `${BASE_URL}${value.berry}`,
-            )
-
-            console.log(response)
-            const resData = await response.json() as BerryRes
-
-            console.log(resData)
+            const url = `${BASE_URL}${value.berry}`
+            return await mutation.mutateAsync(url)
         }
     })
 
     return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault()
-                e.stopPropagation()
-                form.handleSubmit()
-            }}
-        >
-            <form.Field
-                name="berry"
-                children={(field) => (
-                    <>
-                        <Input
-                            name={field.name}
-                            value={field.state.value}
-                            type='text'
-                            onChange={(e) => field.handleChange(e.target.value)}
+        <>
+            {mutation.isSuccess && <p>{mutation.data}</p>}
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    form.handleSubmit()
+                }}
+            >
+                <form.Subscribe
+                    selector={(state) => [state.isSubmitting]}
+                    children={([isSubmitting]) => (
+                        <form.Field
+                            name="berry"
+                            children={(field) => (
+                                <>
+                                    <Input
+                                        disabled={isSubmitting}
+                                        name={field.name}
+                                        value={field.state.value}
+                                        type='text'
+                                        onChange={(e) => field.handleChange(e.target.value)}
+                                    />
+                                    {field.state.meta.errorMap.onChange?.length && (
+                                        field.state.meta.errorMap.onChange.map((fErr) => (<p>{fErr.message}</p>)))
+                                    }
+                                </>
+                            )}
                         />
-                        {field.state.meta.errorMap.onChange?.length && (
-                            field.state.meta.errorMap.onChange.map((fErr) => (<p>{fErr.message}</p>)))
-                        }
-                    </>
-                )}
-            />
-        </form >
+                    )
+                    }
+                />
+                <form.Subscribe
+                    selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    children={([canSubmit, isSubmitting]) => (
+                        <>
+                            <Button type="submit" disabled={!canSubmit}>
+                                {isSubmitting ? '...' : 'Submit'}
+                            </Button>
+                            <Button type="reset" onClick={() => form.reset()}>
+                                Reset
+                            </Button>
+                        </>
+                    )
+                    }
+                />
+            </form >
+        </>
     )
 }
 
